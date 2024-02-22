111df43a44021044dbed3d2524d695b7d725ecbb
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-02-22 14:11:31 +0530

    Rainbow kit connected and working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Rainbowkit Imports
+import "@rainbow-me/rainbowkit/styles.css";
+import './polyfills';
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+//RainbowKit Config
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "7d0e6dd5d018a6b5528a9d2890282e36",
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <App />
+      </RainbowKitProvider>
+    </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
 );
 
