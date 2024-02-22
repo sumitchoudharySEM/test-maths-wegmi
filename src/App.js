56e56c46b0251ae35eba8c7144de9d390233f2b2
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-02-22 15:50:12 +0530

    write contract done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "./App.css";
-import { useAccount, useReadContract } from "wagmi";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import { useEffect, useState } from "react";
 import abi from "./contract/abi.json";
 
 function App() {
-
-  const contractAddress =
-    "0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59";
+  const contractAddress = "0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59";
   const account = useAccount();
+  const [value, setValue] = useState();
 
-  // const {data} = useReadContract({
-  //   contractABI,
-  //   address: contractAddress,
-  //   functionName: 'getNumber',
-  // });
-  const { data :useContractReadData } = useReadContract({
+  const { data: useContractReadData } = useReadContract({
     abi,
-    address: '0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59',
-    functionName: 'getNumber',
+    address: "0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59",
+    functionName: "getNumber",
     watch: true,
-  })
+  });
 
-  useEffect(() => {
-    
-  }, []);
+  const { writeContract } = useWriteContract();
+
+  const setNumber = async () => {
+    writeContract({
+      abi,
+      address: "0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59",
+      functionName: "changeNumber",
+      args: [value],
+    });
+  };
 
   useEffect(() => {
     console.log("__________________________");
@@ -48,7 +49,22 @@ function App() {
       <div>
         <h1>WAGMI</h1>
         <p>Account: {account.address}</p>
-        <p>Number: {useContractReadData? Number(useContractReadData) : "not fetched yet"}</p>
+        <p>
+          Number:{" "}
+          {useContractReadData
+            ? Number(useContractReadData)
+            : "not fetched yet"}
+        </p>
+
+        <input
+          name="value"
+          placeholder="Value"
+          type="number"
+          required
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <p> value: {value} </p>
+        <button onClick={() => setNumber()}>Change Number</button>
       </div>
     </div>
   );
