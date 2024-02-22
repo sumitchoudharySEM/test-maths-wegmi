7937e6560d358eef303289c36b036b7fab4dbf04
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-02-22 18:33:18 +0530

    worked back on polygon mumbai

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import { useEffect, useState } from "react";
 import abio from "./contract/abi.json";
-import abi from "./contract/sepholia.json";
+import abii from "./contract/sepholia.json";
+import abi from "./contract/new.json";
 import { parseEther } from 'viem';
 
 function App() {
   const contractAddress = "0x1705Be686E606FF75Aa4C2F636Cbd8D5f7956b59";
   const contractAddressSepholia = "0x39393F6E21bb435d550e83fdEB32e9Ce7a90C860";
+  const contractAddressNew = "0x5E4d3C33Cd36E4e42E7Cd961b7C3d2906D2F72ba";
   const account = useAccount();
   const [value, setValue] = useState();
 
   const { data: useContractReadData } = useReadContract({
     abi,
-    address: "0x39393F6E21bb435d550e83fdEB32e9Ce7a90C860",
+    address: "0x5E4d3C33Cd36E4e42E7Cd961b7C3d2906D2F72ba",
     functionName: "getNumber",
     watch: true,
   });
@@ -24,7 +26,7 @@ function App() {
   const setNumber = async () => {
     writeContract({
       abi,
-      address: "0x39393F6E21bb435d550e83fdEB32e9Ce7a90C860",
+      address: "0x5E4d3C33Cd36E4e42E7Cd961b7C3d2906D2F72ba",
       functionName: "changeNumber",
       args: [value],
       value: parseEther('0.0001'),
